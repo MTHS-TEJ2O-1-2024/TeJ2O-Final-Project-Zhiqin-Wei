3fe66656ce6e8674c1450765286a1dbea8b56a89
Zhiqin-Wei <zhiqin.daly.wei@mths.ca>
2025-01-21 10:53:00 -0500

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,30 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Zhiqin.wei
+ * Created on: Jan 2025
+ * This program is prize wheel
 */
 
-basic.showString('Hello, World!')
+
+// Happy face at start
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Angry)
+    basic.pause(500)
+    let randomAngle = Math.randomRange(40, 180)
+    basic.showNumber(randomAngle)
+    pins.servoWritePin(AnalogPin.P0, randomAngle)
+    basic.pause(500)
+    basic.clearScreen()
+})
+
+input.onButtonPressed(Button.B, function() {
+    basic.showIcon(IconNames.Confused)
+    basic.pause(500)
+    pins.servoWritePin(AnalogPin.P0, 3)
+    basic.showNumber(0)
+    basic.pause(500)
+    basic.clearScreen()
+})
